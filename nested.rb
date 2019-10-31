05bdf57a590365f3f0160c333cd64bf578eacefc
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-31 17:27:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,6 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     
 programmer_hash[:alan_kay][:known_for] = alans_new_info
 programmer_hash
 end
@@ -127,5 +128,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     dennis_new_info = "Assembly"
 
+programmer_hash[:dennis_ritchie][:languages] = dennis_new_info
+programmer_hash
 end
